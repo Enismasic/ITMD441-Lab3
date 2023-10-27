c846bfafa07d869aafee1ac6c5fabe34a6370515
Enismasic <emasic@hawk.iit.edu>
2023-10-26 20:18:34 -0500

    fixed functionality with typos

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
     //Get references to input elements from index.html
-    const billTototalInput = document.getElementById('billtotal');
+    const billTotalInput = document.getElementById('billtotal');
     const tipSlider = document.getElementById('tip-slider');
     const tipPercentInput = document.getElementById('tip-percent');
     const tipAmountInput = document.getElementById('tip-amount');
     const totalBillWithTipInput = document.getElementById('total-bill-tip');
 
     //Functionality to calculate and update results
-function calculateResults() {
-    const billtotal = parseFloat(billTototalInput.value)
-    const tipPercentage = parseInt(tipSlider.value);
-    const tipAmount = (billTotal * tipPercentage) / 100;
-    const totalBillWithTip = billtotal + tipAmount;
+    function calculateResults() {
+        const billTotal = parseFloat(billTotalInput.value);
+        const tipPercentage = parseInt(tipSlider.value);
+        const tipAmount = (billTotal * tipPercentage) / 100;
+        const totalBillWithTip = billTotal + tipAmount;
 
-    tipPercentInput.value = tipPercentage + '%';
-    tipAmountInput.value = '$' + tipAmount.toFixed(2);
-    totalBillWithTipInput.value = '$' + totalBillWithTip.toFixed(2);
+        tipPercentInput.value = tipPercentage + '%';
+        tipAmountInput.value = '$' + tipAmount.toFixed(2);
+        totalBillWithTipInput.value = '$' + totalBillWithTip.toFixed(2);
+    }
 
-}
-
-    //Event Listeners for bill total and slider 
-    billTototalInput.addEventListener('input', calculateResults);
+     //Event Listeners for bill total and slider
+    billTotalInput.addEventListener('input', calculateResults);
     tipSlider.addEventListener('input', calculateResults);
 
     //calculation
