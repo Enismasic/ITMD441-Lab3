f7e608dfda675f411ee49506cb74a6fc2648265a
Enismasic <emasic@hawk.iit.edu>
2023-10-26 20:00:34 -0500

    added css and js styling/references

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //Get references to input elements from index.html
+    const billTototalInput = document.getElementById('billtotal');
+    const tipSlider = document.getElementById('tip-slider');
+    const tipPercentInput = document.getElementById('tip-percent');
+    const tipAmountInput = document.getElementById('tip-amount');
+    const totalBillWithTipInput = document.getElementById('total-bill-tip');
+
+
+}
